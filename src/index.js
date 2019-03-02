9bc66cab4f5ebe27845b195a28ddd1daeb95434f
Chris Panican <chrispanican@gmail.com>
2019-03-01 21:36:53 -0500

    Add React and ReactDOM

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <p>React Boilerplate with Webpack, Babel, Jest and Eslint.</p>
+  </div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
