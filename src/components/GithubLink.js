614d971bfb1f2b138dc5c7aacec000c82c8c5c2e
Chris Panican <chrispanican@gmail.com>
2019-04-15 22:41:18 -0400

    Put a link to repo on site

--- src/components/GithubLink.js
+++ src/components/GithubLink.js
@@ -4,6 +4,7 @@ import './GithubLink.css';
 const GithubLink = () => (
   <div className="GithubLink">
     <p>Check the code on Github</p>
+    <a className="Button" href="https://github.com/cpanican/react-boilerplate-setup">Github Repo</a>
   </div>
 );
 
