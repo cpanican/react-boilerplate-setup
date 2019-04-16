d7374d0152b4c9eb19a37afb7603d499e9e3f613
Chris Panican <chrispanican@gmail.com>
2019-04-16 18:38:50 -0400

    Prefer single quotes in jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GithubLink from './components/GithubLink';
 import './App.css';
 
 const App = () => (
-  <div className="App">
+  <div className='App'>
     <p>React Boilerplate with Webpack, Babel, Jest and Eslint.</p>
     <GithubLink />
   </div>
--- src/components/GithubLink.js
+++ src/components/GithubLink.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './GithubLink.css';
 
 const GithubLink = () => (
-  <div className="GithubLink">
+  <div className='GithubLink'>
     <p>Check the code on Github</p>
-    <a className="Button" href="https://github.com/cpanican/react-boilerplate-setup">Github Repo</a>
+    <a className='Button' href='https://github.com/cpanican/react-boilerplate-setup'>Github Repo</a>
   </div>
 );
 
