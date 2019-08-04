16ad566d4e4594b73ebf307af34ffe1f58b0c2ec
Chris Panican <chrispanican@gmail.com>
2019-08-04 19:40:21 -0400

    Support images

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: ['babel-polyfill', './src/index.js'],
+  mode: 'development',
   output: {
     publicPath: '/',
   },
-  mode: 'development',
   module: {
     rules: [
       {
@@ -25,6 +25,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
       },
     ],
   },
